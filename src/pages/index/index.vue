7cdf7276b102dc7e4e2fdebb1b4a40766be7c0e2
huangbo <1104133609@qq.com>
2020-07-07 12:22:46 +0800

    master

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -4,25 +4,31 @@
  * @Author: 小白
  * @Date: 2020-07-06 19:27:47
  * @LastEditors: 小白
- * @LastEditTime: 2020-07-06 22:51:55
+ * @LastEditTime: 2020-07-07 12:22:26
 --> 
 <template>
-  <mycontent title="数字旭辉">
-      <cell bind:startmessage='startmessage' bind:completemessage="completemessage" plugid='ea8fa16642f5cefb883c9efd754a2d8e'  />
+  <mycontent title="主页">
+    <cell
+      @startmessage="startmessage"
+      @completemessage="completemessage"
+      plugid="ea8fa16642f5cefb883c9efd754a2d8e"
+    />
   </mycontent>
-  
 </template>
 
 <script lang='ts'>
-import { Component, Prop, Vue } from 'vue-property-decorator';
+import { Component, Prop, Vue } from "vue-property-decorator";
 import mycontent from "@/components/mycontent.vue";
-@Component({components: {mycontent},name:'Index'})
+@Component({ components: { mycontent }, name: "Index" })
 export default class Index extends Vue {
-created() {}
-mounted() {}
+  created() {}
+  mounted() {}
+  completemessage(res:any) {
+   
+  }
+  startmessage() {}
 }
 </script>
 <style lang='scss' scoped>
 /* @import url(); 引入css类 */
-
 </style>
